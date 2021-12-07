b3fb359acf966bd66d1ef38ed7a132909964011a
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2021-12-07 15:41:41 -0300

    Calcular Dollar

---
+++ ConversorDeMoeda/ConversorDeMoeda/CalculadoraDolar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorDeMoeda
+{
+    class CalculadoraDolar
+    {
+        static double IOF = 0.6;
+        static double CalcularCompraDolar(double cotacao, double dolarAComprar)
+        {
+
+        }
+    }
+}
---
+++ ConversorDeMoeda/ConversorDeMoeda/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+namespace ConversorDeMoeda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do Dolar?: ");
+            double cotacao = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            
+            Console.Write("Quantos dolares você irá comprar?");
+            double dolarComprar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+        }
+    }
+}
