f57b921b85af630e0c5eafa7491cfcbd377236f5
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2021-12-07 19:22:54 -0300

    atualização do código DepositoESaque

---
+++ DepositoESaque/DepositoESaque/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com o número da Conta: ");
+            int n_conta = int.Parse(Console.ReadLine());
+            Console.Write("Entre com o título da conta: ");
+            string nome = Console.ReadLine();
+            Console.Write("Haverá depósito inicial? (s/n) ?");
+
+
+
+
+
+
+            char simNao = char.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
---
+++ DepositoESaque/DepositoESaque/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DepositoESaque
+{
+    class Usuario
+    {
+        public int Conta { get; private set; }
+        public string Nome { get; private set; }
+
+        public double Saldo;
+
+        public Usuario(int conta, string nome)
+        {
+            Conta = conta;
+            Nome = nome;
+            Saldo = 0.0;
+
+        }
+
+    }
+}
+
+
+
