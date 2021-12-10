bfd5deff867a156f522b84cde2638cbbea3e18cb
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2021-12-09 22:06:04 -0300

    Concluído

---
+++ ReservaHotel/ReservaHotel/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ReservaHotel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos quartos você deseja reservar? ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Reserva[] x = new Reserva[11]; 
+            
+            for(int i = 1; i <= quantidade; i++ )
+            {
+                Console.WriteLine("Reserva #" + (i) + ":");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+             
+                x[quarto] = new Reserva(nome, email);
+                Console.WriteLine("");
+            }
+
+            Console.WriteLine("Quartos Reservados: ");
+            for (int i = 1; i<=10; i++)
+            {
+                if (x[i] != null)
+                {
+                   Console.WriteLine(i + ": " + x[i]);
+                   
+                    
+                }
+            }
+
+        }
+    }
+}
---
+++ ReservaHotel/ReservaHotel/Reserva.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservaHotel
+{
+    class Reserva
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Reserva(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Nome 
+                + ", "
+                + Email;
+        }
+
+
+    }
+}
