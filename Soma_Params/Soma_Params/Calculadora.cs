b8546d364aa7dc089422018d2c0165b1fe50eca5
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2021-12-10 11:31:24 -0300

    Concluído

---
+++ Soma_Params/Soma_Params/Calculadora.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soma_Params
+{
+    class Calculadora
+    {
+        static public double Soma(params double[] valores)
+        {
+            double soma = 0.0;
+
+            for (int i = 0; i<valores.Length; i++ )
+            {
+                soma = soma + valores[i];
+            }
+
+            return soma;
+
+
+        }
+    }
+}
---
+++ Soma_Params/Soma_Params/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Soma_Params
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*Console.WriteLine("Quantos numeros deseja soma? ");
+            int quantidade = int.Parse(Console.ReadLine());
+            double[] valores = new double[quantidade];
+
+            for (int i = 0 ; i <quantidade;i++ )
+            {
+                valores[i] = double.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(Calculadora.Soma(valores).ToString("F2", CultureInfo.InvariantCulture));*/
+
+
+
+
+            Console.WriteLine(Calculadora.Soma(1,2,3).ToString("F2", CultureInfo.InvariantCulture)); 
+        }
+    }
+}
