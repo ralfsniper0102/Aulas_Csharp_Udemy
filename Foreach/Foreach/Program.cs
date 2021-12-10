dd6e46e9b6f418aa3f7e5e3846f08fb2745011c8
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2021-12-10 12:04:45 -0300

    Concluído

---
+++ Foreach/Foreach/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma quantidade");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Pessoa[] x = new Pessoa[quantidade];
+            
+            for (int i = 0 ; i<quantidade ; i++ )
+            {
+                Console.WriteLine("Nome: ");
+                string nome = Console.ReadLine();
+                Console.WriteLine("Idade: ");
+                int idade = int.Parse(Console.ReadLine());
+
+                x[i] = new Pessoa(nome, idade);
+            }
+
+            foreach(Pessoa obj in x)
+            {
+                Console.WriteLine(obj.Nome + ", "
+                    + obj.Idade + " anos");
+            }
+
+        }
+    }
+}
