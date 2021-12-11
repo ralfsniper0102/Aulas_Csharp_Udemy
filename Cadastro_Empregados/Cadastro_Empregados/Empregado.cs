aac8ff70d321a4f498f24d8a218d3b85d583f4c4
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2021-12-11 13:09:45 -0300

    rever código

---
+++ Cadastro_Empregados/Cadastro_Empregados/Empregado.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastro_Empregados
+{
+    class Empregado
+    {
+        public int Id;
+        public string Nome;
+        public double Salario;
+
+        public Empregado(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+    }
+}
---
+++ Cadastro_Empregados/Cadastro_Empregados/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace Cadastro_Empregados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Empregado> empregados = new List<Empregado>();
+
+            Console.WriteLine("Quantos empregados você irá registrar? ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i<quantidade; i++ )
+            {
+                Console.WriteLine("Empregafo #" + (i+1) +":");
+                int id = 0;
+                do
+                {
+                    Console.Write("Id: ");
+                    id = int.Parse(Console.ReadLine());
+                    List<Empregado> resultado = new List<>();
+
+
+
+                } while (); 
+
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Salario: ");
+                double salario = double.Parse(Console.ReadLine());
+
+                empregados.Add(new Empregado(id, nome, salario));
+            }
+
+            Console.WriteLine("insira o id do funcionario que voce deseja aumentar o salario: ");
+            int id = int.Parse(Console.ReadLine());
+
+
+
+        }
+    }
+}
