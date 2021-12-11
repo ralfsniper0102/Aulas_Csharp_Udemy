eccef902ec9be49d853b5f220b07514ade3ee34b
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2021-12-11 10:12:56 -0300

    Incompleto

---
+++ Listas/Listas/Pessoa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listas
+{
+    class Pessoa
+    {
+        public string Nome;
+        public int Idade;
+
+        public Pessoa(string nome, int idade)
+        {
+            Nome = nome;
+            Idade = idade;
+        }
+    }
+}
---
+++ Listas/Listas/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace Listas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Pessoa> lista;
+
+            Console.WriteLine("Digite o Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite a Idade: ");
+            int idade = int.Parse(Console.ReadLine());
+
+            lista.Add()
+        }
+    }
+}
