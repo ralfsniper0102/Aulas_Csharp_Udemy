bcfe58537d0dba7eeaf87943e43c4c36bc68f0b4
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2021-12-11 11:43:38 -0300

    Concluído
    Instanciação de Classes em lista

--- Listas/Listas/Program.cs
+++ Listas/Listas/Program.cs
@@ -8,15 +8,29 @@ namespace Listas
     {
         static void Main(string[] args)
         {
-            List<Pessoa> lista;
+            List<Pessoa> lista = new List<Pessoa>();
 
-            Console.WriteLine("Digite o Nome: ");
-            string nome = Console.ReadLine();
 
-            Console.WriteLine("Digite a Idade: ");
-            int idade = int.Parse(Console.ReadLine());
+            Console.WriteLine("Quantas pessoas deseja cadastrar? ");
+            int quantidade = int.Parse(Console.ReadLine());
 
-            lista.Add()
+            for(int i = 0; i<quantidade ; i++)
+            {
+                Console.WriteLine("Digite o Nome: ");
+                string nome = Console.ReadLine();
+
+                Console.WriteLine("Digite a Idade: ");
+                int idade = int.Parse(Console.ReadLine());
+
+                lista.Add(new Pessoa(nome, idade));
+            }
+
+            foreach (Pessoa obj in lista)
+            {
+                Console.WriteLine(obj.Nome + ", " + obj.Idade);
+            }
+            
+       
         }
     }
 }
