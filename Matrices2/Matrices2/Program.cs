09c0d407443e720d238e88788f5717f84116449f
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2021-12-14 23:41:28 -0300

    Concluded

--- Matrices2/Matrices2/Program.cs
+++ Matrices2/Matrices2/Program.cs
@@ -12,15 +12,50 @@ namespace Matrices2
 
             var matrix_main = new int[(int.Parse(vlInput[0])), (int.Parse(vlInput[1]))];
 
-            
+
             for (int line = 0; line < int.Parse(vlInput[0]); line++)
             {
                 var vlInputMatrix = Console.ReadLine().Split();
                 int zero = 0;
-                for (int column = zero; column < int.Parse(vlInput[1]); column = column +1)
+                for (int column = zero; column < int.Parse(vlInput[1]); column = column + 1)
                 {
-                    
+
                     matrix_main[line, column] = int.Parse(vlInputMatrix[column]);
+                }
+            }
+
+            int vlInputScan = int.Parse(Console.ReadLine());
+            for (int line = 0; line < int.Parse(vlInput[0]); line++)
+            {
+
+                int zero = 0;
+                for (int column = zero; column < int.Parse(vlInput[1]); column = column + 1)
+                {
+                    if (matrix_main[line, column] == vlInputScan)
+                    {
+
+                        Console.WriteLine("Position: " + line + "," + column);
+
+                        if (column != 0)
+                        {
+                            Console.WriteLine("Left: " + matrix_main[line, (column - 1)]);
+                        }
+
+                        if (column != int.Parse(vlInput[1]) - 1)
+                        {
+                            Console.WriteLine("Right: " + matrix_main[line, (column + 1)]);
+                        }
+
+                        if (line != 0)
+                        {
+                            Console.WriteLine("Up: " + matrix_main[(line - 1), column]);
+                        }
+
+                        if (line != int.Parse(vlInput[0]))
+                        {
+                            Console.WriteLine("Down: " + matrix_main[(line + 1), column]);
+                        }
+                    }
                 }
             }
         }
