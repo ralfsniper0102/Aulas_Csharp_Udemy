a8427a8aecd837cc30f98f3d98ced140dbe5e155
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2021-12-15 15:51:30 -0300

    Incomplete

---
+++ Reuse/Reuse/PrintService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reuse
+{
+    class PrintService<T> ///// modificated for reuse
+    {
+        private T[] _values = new T[10]; ///// modificated for reuse
+        private int _count = 0;
+
+        public void addValue(T value) ///// modificated for reuse
+        {
+            if (_count == 10)
+            {
+                throw new InvalidOperationException("PrintService Cheio");
+            }
+            _values[_count] = value;
+            _count++;
+        }
+
+
+        public T First() ///// modificated for reuse
+        {
+            if (_count == 0)
+            {
+                throw new InvalidOperationException("PrintService Vazio");
+            }
+            return (_values[0]);
+        }
+
+        public void Print()
+        {
+            if (_count == 0)
+            {
+                throw new InvalidOperationException("PrintService Vazio");
+            }
+            Console.Write("[");
+            for (int count = 0; count < (_count - 1); count++)
+            {
+                Console.Write(_values[count].ToString() + ", ");
+            }
+            Console.Write(_values[(_count - 1)]);
+            Console.Write("]");
+        }
+    }
+}
---
+++ Reuse/Reuse/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Reuse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var printService = new PrintService<string>(); ///// modificated for reuse
+
+            Console.WriteLine("Quantos valores voce deseja inserir? ");
+            int quantity = int.Parse(Console.ReadLine());
+
+            for (int count = 0; count < quantity; count++)
+            {
+                printService.addValue = Console.ReadLine();
+            }
+
+            printService.Print();
+            Console.WriteLine("");
+            Console.WriteLine("First: " + printService.First().ToString());
+
+
+
+        }
+    }
+}
