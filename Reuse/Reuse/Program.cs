ff2eaacb9e537898e26b29dbd813e656796e0b86
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2021-12-16 16:20:50 -0300

    Completed

--- Reuse/Reuse/Program.cs
+++ Reuse/Reuse/Program.cs
@@ -7,14 +7,14 @@ namespace Reuse
         static void Main(string[] args)
         {
 
-            var printService = new PrintService<string>(); ///// modificated for reuse
+            var printService = new PrintService<int>(); ///// modificated for reuse
 
             Console.WriteLine("Quantos valores voce deseja inserir? ");
             int quantity = int.Parse(Console.ReadLine());
 
             for (int count = 0; count < quantity; count++)
             {
-                printService.addValue = Console.ReadLine();
+                printService.addValue(int.Parse(Console.ReadLine())); 
             }
 
             printService.Print();
