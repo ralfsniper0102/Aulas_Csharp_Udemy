4d39bab1448d69fb1c5d034abeb2796e30a51793
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2022-01-12 22:40:26 -0300

    completed

---
+++ EnumProgram/EnumProgram/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace EnumProgram.Entities.Enums
+{
+    enum OrderStatus : int
+    {
+        PagamentoPendente = 0,
+        Processando = 1,
+        ACaminho = 2,
+        Entregue = 3
+
+    }
+}
---
+++ EnumProgram/EnumProgram/Program.cs
@@ -0,0 +1,29 @@
+﻿using EnumProgram.Entities;
+using System;
+using EnumProgram.Entities.Enums;
+
+namespace EnumProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PagamentoPendente
+                
+
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.Entregue.ToString();
+            Console.WriteLine(txt);
+
+            OrderStatus orderStatus = Enum.Parse<OrderStatus>("Entregue");
+            Console.WriteLine(orderStatus);
+        }
+    }
+}
