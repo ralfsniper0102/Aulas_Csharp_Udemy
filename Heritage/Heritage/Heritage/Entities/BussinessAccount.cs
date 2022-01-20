7e108c34d02d8ab6dc007592780adee8a6ddb9fe
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2022-01-20 14:48:14 -0300

    Incompleted

---
+++ Heritage/Heritage/Heritage/Entities/BussinessAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heritage.Entities
+{
+    class BussinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BussinessAccount(double loanLimit, int number, string holder, double balance)
+            : base(number, holder, balance)    
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            Balance = amount;
+        }
+    }
+}
