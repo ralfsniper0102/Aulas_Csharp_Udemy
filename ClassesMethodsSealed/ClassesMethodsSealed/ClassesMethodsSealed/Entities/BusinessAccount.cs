1c8b2d4e6d1058f28225ab5582e888a77a2583a2
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2022-01-21 23:58:15 -0300

    Completed

---
+++ ClassesMethodsSealed/ClassesMethodsSealed/ClassesMethodsSealed/Entities/BusinessAccount.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpcastingDowncasting.Entities
+{
+    class BusinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount()
+        {
+
+        }
+
+        public BusinessAccount(double loanLimit, int number, string holder, double balance)
+            : base(number, holder, balance)
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if (amount <= LoanLimit)
+            {
+                Balance = Balance + amount;
+            }
+
+
+        }
+
+        public sealed override void Withdraw(double amount)  // sealed method allowed only to override methods
+        {
+            base.Withdraw(amount);
+            Balance = Balance - 2.00;
+        }
+    }
+}
