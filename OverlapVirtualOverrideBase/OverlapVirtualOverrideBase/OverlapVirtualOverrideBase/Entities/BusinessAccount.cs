35a2fb2d668aa3e9c1a6bd262327d3d886089f43
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2022-01-21 23:17:34 -0300

    Completed

---
+++ OverlapVirtualOverrideBase/OverlapVirtualOverrideBase/OverlapVirtualOverrideBase/Entities/BusinessAccount.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpcastingDowncasting.Entities
+{
+    class BusinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount()
+        {
+
+        }
+
+        public BusinessAccount(double loanLimit, int number, string holder, double balance)
+            : base(number, holder, balance)
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if (amount <= LoanLimit)
+            {
+                Balance = Balance + amount;
+            }
+
+
+        }
+        public override void Withdraw(double amount)
+        {
+            base.Withdraw(amount); //reuse
+            Balance = Balance - 2.00;
+        }
+    }
+}
---
+++ OverlapVirtualOverrideBase/OverlapVirtualOverrideBase/OverlapVirtualOverrideBase/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UpcastingDowncasting.Entities;
+
+namespace OverlapVirtualOverrideBase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc1 = new Account(2132,"Rafael", 10000);
+            Account acc2 = new BusinessAccount(10000, 32324, "Rebeca", 10000);
+
+            acc1.Withdraw(10);
+            acc2.Withdraw(10);
+
+            Console.WriteLine(acc1.Balance);
+            Console.WriteLine(acc2.Balance);
+        }
+    }
+}
