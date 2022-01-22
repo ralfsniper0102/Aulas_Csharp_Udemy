06d54e2442f8619b7e26f18d5146f1f48dd50a12
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2022-01-21 22:43:24 -0300

    completed

--- UpcastingDowncasting/UpcastingDowncasting/UpcastingDowncasting/Program.cs
+++ UpcastingDowncasting/UpcastingDowncasting/UpcastingDowncasting/Program.cs
@@ -14,7 +14,7 @@ namespace UpcastingDowncasting
 
             Account acc1 = bacc;
             Account acc2 = new BusinessAccount(200.0, 1003, "Bob",0.0);
-            Account acc3 = new BusinessAccount(0.01, 1004, "Anna", 0.0);
+            Account acc3 = new SavingsAccount(0.01, 1004, "Anna", 0.0);
 
 
             //DOWNCASTING
@@ -22,17 +22,23 @@ namespace UpcastingDowncasting
             BusinessAccount acc4 = (BusinessAccount)acc2;
             acc4.Loan(100.0); 
 
-            if (acc3 is BusinessAccount) //// teste 
+            if (acc3 is BusinessAccount) //// test 
             {
-                BusinessAccount acc5 = (BusinessAccount)acc3;
+                //BusinessAccount acc5 = (BusinessAccount)acc3;
+                BusinessAccount acc5 = acc3 as BusinessAccount;// other way to casting
+                acc5.Loan(200.00);
+                Console.WriteLine("Loan!");
             }
 
-            if (acc3 is SavingsAccount)
+            if (acc3 is SavingsAccount) //test
             {
-
+                //SavingsAccount acc5 = (SavingsAccount)acc3;
+                SavingsAccount acc5 = acc3 as SavingsAccount;// other way to casting
+                acc5.UpdateBalance();
+                Console.WriteLine("Update!");
             }
 
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
