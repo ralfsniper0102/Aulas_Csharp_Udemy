60ce77e75d2521fc9fa65d06efe159698c010f53
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2022-01-24 23:12:21 -0300

    Completed

---
+++ AbstractClasses/AbstractClasses/AbstractClasses/Entities/BussinesAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses.Entities
+{
+    class BussinesAccount : Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BussinesAccount()
+        {
+
+        }
+
+        public BussinesAccount(int number, string holder, double balance, double loanLimit)
+            : base (number, holder, balance)
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if (amount <= LoanLimit)
+            {
+                Balance = Balance + amount;
+            }
+        }
+    }
+}
