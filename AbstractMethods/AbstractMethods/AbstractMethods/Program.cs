953823bf3d9e4bf4a12122afaa7d5057c0d50cd5
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2022-01-25 18:43:28 -0300

    incompleted

--- AbstractMethods/AbstractMethods/AbstractMethods/Program.cs
+++ AbstractMethods/AbstractMethods/AbstractMethods/Program.cs
@@ -19,22 +19,36 @@ namespace AbstractMethods
                 Console.WriteLine("Dados da forma #" + (i + 1) + ":");
                 Console.Write("Retangulo ou Circulo (r/c)? ");
                 char typeForm = char.Parse(Console.ReadLine().ToLower());
-
-                if (typeForm == 'r')
+                Color color;
+                switch (typeForm)
                 {
-                    Console.Write("Cor (Preto/Azul/Vermelho)");
-                    Color color = Enum.Parse<Color>(Console.ReadLine());
-                    Console.WriteLine("Largura: ");
-                    double width = double.Parse(Console.ReadLine());
-                    Console.Write("Altura: ");
-                    double height = double.Parse(Console.ReadLine());
+                    case 'r':
+                        Console.Write("Cor (Preto/Azul/Vermelho)");
+                        color = Enum.Parse<Color>(Console.ReadLine());
+                        Console.WriteLine("Largura: ");
+                        double width = double.Parse(Console.ReadLine());
+                        Console.Write("Altura: ");
+                        double height = double.Parse(Console.ReadLine());
 
-                    shapes.Add(new Rectangle(width, height, color));
-                }
-                else
-                {
+                        shapes.Add(new Rectangle(width, height, color));
+                        break;
+
+                    case 'c':
+                        Console.Write("Cor (Preto/Azul/Vermelho)");
+                        color = Enum.Parse<Color>(Console.ReadLine());
+
+
+                        break;
+
+                    default:
+                        break;
+
 
                 }
+                    
+                
+                    
+               
             }
         }
     }
