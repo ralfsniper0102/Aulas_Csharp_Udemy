5a1655e2cc778afaac9a4a95a28cabe964c11710
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2022-01-27 18:08:38 -0300

    completed

---
+++ TryCatchProgram/TryCatchProgram/TryCatchProgram/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TryCatchProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+          
+            try
+            {
+                Console.WriteLine("ola digite um numero");
+                double a = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(a);
+
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("erro " + ex.Message);
+            }
+        }
+    }
+}
