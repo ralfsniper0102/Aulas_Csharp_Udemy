5eabcdaec6f461a17c41e4151c318f9512677a2b
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2022-01-28 22:04:41 -0300

    completed

---
+++ ProgramHotel/ProgramHotel/ProgramHotel/Entities/Reservation.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProgramHotel.Entities.Exceptions;
+
+namespace ProgramHotel.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        Reservation()
+        {
+
+        }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            if (checkOut<= checkIn)
+            {
+                throw new DomainException("O check-out deve ser posterior a data de check-in");
+            }
+
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public void UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            DateTime now = DateTime.Now;
+
+            if (checkIn > now || checkOut > now)
+            {
+                throw new DomainException("Para ser atualizado é necessário as datas serem futuras");
+
+            }
+            if (checkOut <= checkIn)
+            {
+                throw new DomainException("O check-out deve ser posterior a data de check-in");
+            }
+             
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(
+                "Quarto: " + RoomNumber
+                + ", check-in: "
+                + CheckIn.ToString("dd/MM/yyyy")
+                + ", check-out: "
+                + CheckOut.ToString("dd/MM/yyyy, ")
+                + Duration()
+                );
+            if (Duration() > 1)
+            {
+                sb.Append(" noites");
+            }
+            else
+            {
+                sb.Append(" noite");
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ ProgramHotel/ProgramHotel/ProgramHotel/Program.cs
@@ -0,0 +1,65 @@
+﻿using ProgramHotel.Entities;
+using System;
+using System.Collections.Generic;
+using ProgramHotel.Entities.Exceptions;
+
+namespace ProgramHotel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            try
+            {
+
+                Console.Write("número do quarto: ");
+                int roomNumber = int.Parse(Console.ReadLine());
+
+                Console.Write("Data do ckeck-in (dd/mm/aaaa): ");
+                DateTime checkIn = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("Data do ckeck-out (dd/mm/aaaa): ");
+                DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+                Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+
+
+                Console.WriteLine(reservation.ToString());
+
+
+                Console.WriteLine("Entre com os dados para atualizar a reserva: ");
+
+
+                Console.Write("Data do ckeck-in (dd/mm/aaaa): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("Data do ckeck-out (dd/mm/aaaa): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                reservation.UpdateDates(checkIn, checkOut);
+
+                Console.WriteLine(reservation.ToString());
+            }
+
+            catch (DomainException e)
+            {
+                Console.WriteLine("Erro na reserva: " + e.Message);
+                
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Erro: " + e.Message);
+            }
+            /*catch (Exception e) /////general exception
+            {
+                Console.WriteLine(e.Message);
+            }*/
+
+
+        }
+    }
+}
