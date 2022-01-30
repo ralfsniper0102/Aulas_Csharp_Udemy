236d3388e408e63d2c19c02773020341f0583ba1
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2022-01-30 16:12:12 -0300

    Completed

---
+++ FileStreamStreamReader/FileStreamStreamReader/FileStreamStreamReader/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace FileStreamStreamReader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Rafael\OneDrive\Área de Trabalho\File1.txt";
+            StreamReader sr = null;
+
+            try
+            {
+                sr = File.OpenText(path);
+
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+
+                    Console.WriteLine(line);
+                }
+                
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Um erro ocorreu");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null)
+                {
+                    sr.Close();
+                }
+
+                
+            }
+        }
+    }
+}
