3fe2a42bca697192ecbe786b3dc391f11d9f890d
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2022-02-06 13:22:29 -0300

    Completed

---
+++ BlocoUsingProgram/BlocoUsingProgram/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace BlocoUsingProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Rafael\OneDrive\Área de Trabalho\File1.txt";
+
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ocorreu um erro,  " + ex.Message);
+            }
+
+            try
+            {
+
+            }
+            catch
+            {
+
+            }
+
+        }
+    }
+}
