8446782ed80e58e4f4059ea6fe35d754d1a85981
Rafael Araujo da Silva <ralfsniper0102@gmail.com>
2022-02-08 14:53:51 -0300

    Incompleted

---
+++ InterfaceProgram/InterfaceProgram/InterfaceProgram/Domain/Invoice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceProgram.Entities
+{
+    class Invoice
+    {
+        public double BasicPayment { get; set; }
+        public double Tax { get; set; }
+
+        public double TotalPayment()
+        {
+            return BasicPayment + Tax;
+        }
+    }
+}
---
+++ InterfaceProgram/InterfaceProgram/InterfaceProgram/Services/CrudRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceProgram.Services
+{
+    class CrudRepository 
+    {
+        public object Save(object T)
+        {
+            return T;
+        }
+
+        public void Delete(object T)
+        {
+         
+        }
+
+        public  FindById()
+        {
+            return T;
+        }
+
+
+    }
+}
